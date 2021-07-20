6c876fb61a3259b06b43becc8013bcd8b9dafddc
paladinescamila <camilapaladines27@gmail.com>
2021-07-19 19:13:58 -0500

    Readed task db and show them

--- js/index.js
+++ js/index.js
@@ -1,7 +1,49 @@
 const secondsContainer = document.getElementById("seconds");
 
+const workButton = document.getElementById("work-button");
+const shortBreakButton = document.getElementById("short-break-button");
+const longBreakButton = document.getElementById("long-break-button");
+
+const noStartedContainer = document.getElementById("no-started");
+const inProgressContainer = document.getElementById("in-progress");
+const completedContainer = document.getElementById("completed");
+
 // let time = 1;
 // setInterval(() => {
 // 	secondsContainer.innerHTML = time;
 // 	time++;
 // }, 1000);
+
+workButton.addEventListener("click", (e) => {
+	workButton.style.borderBottom = "3px solid var(--white)";
+	shortBreakButton.style.borderBottom = "3px solid transparent";
+	longBreakButton.style.borderBottom = "3px solid transparent";
+});
+
+shortBreakButton.addEventListener("click", (e) => {
+	workButton.style.borderBottom = "3px solid transparent";
+	shortBreakButton.style.borderBottom = "3px solid var(--white)";
+	longBreakButton.style.borderBottom = "3px solid transparent";
+});
+
+longBreakButton.addEventListener("click", (e) => {
+	workButton.style.borderBottom = "3px solid transparent";
+	shortBreakButton.style.borderBottom = "3px solid transparent";
+	longBreakButton.style.borderBottom = "3px solid var(--white)";
+});
+
+let noStartedTasks = tasks.filter((t) => t.status === 1);
+let inProgressTasks = tasks.filter((t) => t.status === 2);
+let completedTasks = tasks.filter((t) => t.status === 3);
+
+const fillTasks = (tasksList, tasksContainer) => {
+	let tasksHTML = "";
+	for (let i = 0; i < tasksList.length; i++) {
+		tasksHTML += `<li><input type="checkbox" ${tasksList[i].status === 3 ? "checked" : ""} /> ${tasksList[i].description}</li>`;
+	}
+	tasksContainer.innerHTML = tasksHTML;
+};
+
+fillTasks(noStartedTasks, noStartedContainer);
+fillTasks(inProgressTasks, inProgressContainer);
+fillTasks(completedTasks, completedContainer);
---
+++ js/tasks.js
@@ -0,0 +1,42 @@
+let tasks = [
+	{
+		description: "This is my first task",
+		status: 1,
+		pomodoros: 2,
+	},
+	{
+		description: "This is my  task",
+		status: 3,
+		pomodoros: 2,
+	},
+	{
+		description: "This is my no task",
+		status: 1,
+		pomodoros: 2,
+	},
+	{
+		description: "This is my  task",
+		status: 2,
+		pomodoros: 2,
+	},
+	{
+		description: "This is my  task",
+		status: 3,
+		pomodoros: 2,
+	},
+	{
+		description: "This is my  task",
+		status: 3,
+		pomodoros: 2,
+	},
+	{
+		description: "This is my  task",
+		status: 2,
+		pomodoros: 2,
+	},
+	{
+		description: "This is my algo task",
+		status: 1,
+		pomodoros: 2,
+	},
+];
