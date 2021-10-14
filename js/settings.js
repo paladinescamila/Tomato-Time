79de3dd99e86ff5b1575ee92eee2f0c15e7df274
paladinescamila <camilapaladines27@gmail.com>
2021-10-14 16:40:31 -0500

    Added local storage for settings data

--- js/settings.js
+++ js/settings.js
@@ -1,14 +1,12 @@
-// Minutes
-let workTime = 25,
-	shortBreakTime = 5,
-	longBreakTime = 10;
+// General
+localStorage.setItem("workTime", 25);
+localStorage.setItem("shortBreakTime", 5);
+localStorage.setItem("longBreakTime", 10);
+localStorage.setItem("autoStartWork", true);
+localStorage.setItem("autoStartBreaks", true);
+localStorage.setItem("longBreakInterval", 4);
 
 // Colors
 let workColor = "#E74545",
 	shortBreakColor = "#1a9aa3",
 	longBreakColor = "#456eb5";
-
-// Others
-let autoStartWork = true,
-	autoStartBreaks = true,
-	longBreakInterval = 4;
