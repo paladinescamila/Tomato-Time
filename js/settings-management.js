ed3101a72f73d535f79f4596eb61316794a89ab3
paladinescamila <camilapaladines27@gmail.com>
2021-10-24 16:06:46 -0500

    Added color picker

--- js/settings-management.js
+++ js/settings-management.js
@@ -13,6 +13,10 @@ const autoWorkValue = document.getElementById("auto-work-value");
 const autoBreakValue = document.getElementById("auto-break-value");
 const cancelSettingsButton = document.getElementById("cancel-settings");
 const saveSettingsButton = document.getElementById("save-settings");
+const colorPicker = document.getElementById("color-picker");
+
+// Variables
+let themeToChange = 1;
 
 // Open settings window
 settingsButton.addEventListener("click", (e) => {
@@ -72,3 +76,60 @@ saveSettingsButton.addEventListener("click", (e) => {
 
 	displaySettings.style.display = "none";
 });
+
+// Change color theme (work)
+workColorButton.addEventListener("click", (e) => {
+	colorPicker.style.display = "grid";
+	document.querySelector(":root").style.setProperty("--show-work", 1);
+	document.querySelector(":root").style.setProperty("--show-short", 0);
+	document.querySelector(":root").style.setProperty("--show-long", 0);
+	document.querySelector(":root").style.setProperty("--picker-left", "90px");
+	themeToChange = 1;
+});
+
+// Change color theme (short break)
+shortColorButton.addEventListener("click", (e) => {
+	colorPicker.style.display = "grid";
+	document.querySelector(":root").style.setProperty("--show-work", 0);
+	document.querySelector(":root").style.setProperty("--show-short", 1);
+	document.querySelector(":root").style.setProperty("--show-long", 0);
+	document.querySelector(":root").style.setProperty("--picker-left", "260px");
+	themeToChange = 2;
+});
+
+// Change color theme (lonk break)
+longColorButton.addEventListener("click", (e) => {
+	colorPicker.style.display = "grid";
+	document.querySelector(":root").style.setProperty("--show-work", 0);
+	document.querySelector(":root").style.setProperty("--show-short", 0);
+	document.querySelector(":root").style.setProperty("--show-long", 1);
+	document.querySelector(":root").style.setProperty("--picker-left", "390px");
+	themeToChange = 3;
+});
+
+// Pick a color
+let colorKeys = ["red", "cyan", "blue", "green", "orange", "pink"];
+let colorValues = ["#e74545", "#1a9aa3", "#456eb5", "#1aa355", "#dbac1f", "#db1fbf"];
+
+for (let i = 0; i < colorKeys.length; i++) {
+	document.getElementById(`${colorKeys[i]}-bg`).addEventListener("click", (e) => {
+		if (themeToChange === 1) {
+			workColor = colorValues[i];
+			workColorButton.style.backgroundColor = workColor;
+			document.querySelector(":root").style.setProperty("--show-work", 0);
+		}
+		if (themeToChange === 2) {
+			shortBreakColor = colorValues[i];
+			shortColorButton.style.backgroundColor = shortBreakColor;
+			document.querySelector(":root").style.setProperty("--show-short", 0);
+		}
+		if (themeToChange === 3) {
+			longBreakColor = colorValues[i];
+			longColorButton.style.backgroundColor = longBreakColor;
+			document.querySelector(":root").style.setProperty("--show-long", 0);
+		}
+
+		colorPicker.style.display = "none";
+		if (themeToChange === timeType) document.querySelector(":root").style.setProperty("--theme-color", colorValues[i]);
+	});
+}
